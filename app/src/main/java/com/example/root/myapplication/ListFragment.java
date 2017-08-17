e007cf671ed2ce3ba8aa2c4f3a00b15b16f01372
string-saqib <s.sarwar@string.org.in>
2017-08-17 17:09:49 +0530

    Data not displaying on second screen

--- app/src/main/java/com/example/root/myapplication/ListFragment.java
+++ app/src/main/java/com/example/root/myapplication/ListFragment.java
@@ -6,25 +6,26 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.ListView;
+import android.support.v7.widget.RecyclerView;
+
 
 import java.util.ArrayList;
 
 
 public class ListFragment extends Fragment {
 
-    GridView gridView;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     onButtonClickListener buttonClickListener;
 
-    private static MyAdapter adapter;
-
-    public interface onButtonClickListener{
-        public void onButtonClick();
+    interface onButtonClickListener{
+        void onButtonClick();
     }
 
     public void onCreate(Bundle savedInstanceState) {
@@ -43,14 +44,17 @@ public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View inflateView = inflater.inflate(R.layout.fragment_list, container, false);
-        gridView = inflateView.findViewById(R.id.grid_view);
-        adapter = new MyAdapter(getArguments().<StudentInfo>getParcelableArrayList("StudentsList"), getContext());
-        gridView.setAdapter(adapter);
+
+        View inflateView = inflater.inflate(R.layout.fragment_list,container,false);
+        mRecyclerView = inflateView.findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new MyAdapter(getArguments().<StudentInfo>getParcelableArrayList("StudentsList"));
+        mRecyclerView.setAdapter(mAdapter);
 
         Button button = inflateView.findViewById(R.id.back_button);
         button.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 buttonClickListener.onButtonClick();
--- app/src/main/java/com/example/root/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/root/myapplication/MyAdapter.java
@@ -1,42 +1,49 @@
 package com.example.root.myapplication;
 
-import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MyAdapter extends ArrayAdapter<StudentInfo> {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
-    ArrayList<StudentInfo> arrayList;
-    Context mContext;
+    ArrayList<StudentInfo> studentList;
 
-    public MyAdapter(ArrayList<StudentInfo> listinfo, Context context){
-        super(context,R.layout.simple_list_item,listinfo);
-        arrayList = listinfo;
-        mContext = context;
+    public MyAdapter(ArrayList<StudentInfo> listinfo){
+        studentList = listinfo;
     }
 
-    private static class ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder{
         TextView txtName;
         TextView txtAgeAndSex;
+        View layout;
+
+        public ViewHolder(View v){
+            super(v);
+            layout = v;
+            txtName = v.findViewById(R.id.detail1);
+            txtAgeAndSex = v.findViewById(R.id.detail2);
+        }
     }
 
-    public View getView(int position, View convertView, ViewGroup container){
-
-        ViewHolder viewHolder = new ViewHolder();
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        convertView = inflater.inflate(R.layout.simple_list_item,container,false);
-        viewHolder.txtName =  convertView.findViewById(R.id.detail1);
-        viewHolder.txtAgeAndSex =  convertView.findViewById(R.id.detail2);
-
-            viewHolder.txtName.setText(arrayList.get(position).getName());
-            viewHolder.txtAgeAndSex.setText(arrayList.get(position).getAge() + ", " + arrayList.get(position).getGender());
-
-        return convertView;
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType){
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.simple_list_item, parent,false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
     }
+
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.txtName.setText(studentList.get(position).getName());
+        holder.txtAgeAndSex.setText(studentList.get(position).getAge() + ", " + studentList.get(position).getGender());
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
 }
